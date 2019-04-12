a680d9ecdcffa262a128205595844ad16626d9e6
syn <vicdemilly@gmail.com>
2019-04-12 10:59:29 +0200

    Ajout de commentaires

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var mdp = document.getElementById('password');
 var mdpconfirm = document.getElementById('confirmPassword');
 var verif = document.getElementById('verification');
 var textzone = document.getElementsByClassName('mdp');
-
+// Plus bas, la variable verif (bouton) produit un événement lors du click sur celui-ci
+// Dans ce cas, il affiche des bordures rouges autour des zones de textes si le mot de passe n'est pas indentique lors de la confirmation
+// Au contraire, ces bordures sont vertes si les mots de passe sont identiques
 verif.addEventListener('click',function(){
   if (mdp.value === mdpconfirm.value){
     mdp.style.border='solid green 3px';
